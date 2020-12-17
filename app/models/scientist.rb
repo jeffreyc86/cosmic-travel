fed2f2721302bd9ca92d89dceb8f8d5aac2095fc
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-12-17 15:52:52 -0500

    complete

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,6 +1,6 @@
 class Scientist < ApplicationRecord
 
-    has_many :missions
+    has_many :missions, dependent: :delete_all
     has_many :planets, through: :missions
     
     validates :name, :field_of_study, presence: true
